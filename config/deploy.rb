f0c426eb763c6a2dbfe398b7f75b7206f5141fac
Cheikh Ndiongue <Cheikhndiongue3@gmail.com>
2017-11-25 10:50:08 -0500

    added bundler and deleted some files

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,11 +39,11 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
-  on roles(:web), in: :groups, limit: 3, wait: 10 do
-
-  end
-end
+# namespace :deploy do
+#   on roles(:web), in: :groups, limit: 3, wait: 10 do
+#
+#   end
+# end
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
